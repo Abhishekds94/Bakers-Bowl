691c4a1048b22689ce8765273ecfbf15a95795fd
Abhishek D S <abhishekds94@gmail.com>
2020-02-21 14:43:57 -0600

    dashboard

--- app/src/main/java/com/abhishek/bakingapp/DashboardActivity.java
+++ app/src/main/java/com/abhishek/bakingapp/DashboardActivity.java
@@ -11,6 +11,14 @@ public class DashboardActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //To make full screen for splash screen
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
         setContentView(R.layout.activity_dashboard);
 
     }
