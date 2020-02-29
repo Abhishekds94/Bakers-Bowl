4937f1e67c8f5ae9a39151328ea77d8445ec978e
Abhishek D S <abhishekds94@gmail.com>
2020-02-28 22:07:12 -0600

    added parceleble

--- app/src/main/java/com/abhishek/bakingapp/model/Recipe.java
+++ app/src/main/java/com/abhishek/bakingapp/model/Recipe.java
@@ -48,8 +48,8 @@ public class Recipe implements Parcelable
     protected Recipe(Parcel in) {
         this.id = ((Integer) in.readValue((Integer.class.getClassLoader())));
         this.name = ((String) in.readValue((String.class.getClassLoader())));
-        in.readList(this.ingredients, (com.abhishek.bakingapp.Ingredient.class.getClassLoader()));
-        in.readList(this.steps, (com.abhishek.bakingapp.Step.class.getClassLoader()));
+        in.readList(this.ingredients, (com.abhishek.bakingapp.model.Ingredient.class.getClassLoader()));
+        in.readList(this.steps, (com.abhishek.bakingapp.model.Step.class.getClassLoader()));
         this.servings = ((Integer) in.readValue((Integer.class.getClassLoader())));
         this.image = ((String) in.readValue((String.class.getClassLoader())));
     }
